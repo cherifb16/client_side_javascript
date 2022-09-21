81069e061dd3a4e9606296ef5f9e385710174a2e
cherifb16 <cherifbobbyray@gmail.com>
2022-09-21 18:55:02 +0200

    test

---
+++ Z_Jquerry_exercices/javascripts/sample.js
@@ -0,0 +1,41 @@
+// $(function(){
+//   $("h1").append("pour démarrer.");
+// });
+
+
+// $(function(){
+//   $("div").append("<p>À propos de jQuery</p>");
+// });
+
+// $(function(){
+//   $("div").prepend("<p>À propos de jQuery</p>");
+// });
+
+// $(function(){
+//   $("<p>À propos de jQuery</p>").appendTo("div");
+// });
+
+
+
+
+
+// $(function(){
+//   $("#btn").on("click", function() {
+//     $(this).hide("500", "linear")
+//   });
+// });
+
+
+// $(function(){
+//   $("#btn").on("click", function() {
+//     $("p").show("500", "linear")
+//   });
+// });
+
+
+
+// $(function(){
+//   $("#btn").on("click", function() {
+//     $("p").toggle("500", "linear")
+//   });
+// });
